eacf9bcd202ac803ae5ebda2f50b70f1e6fc8ab4
sihc <tvquocchi@gmail.com>
2020-04-22 14:02:45 +0700

    chore: remove unused file

--- .build/src/notification_mqtt/send_message.py
+++
@@ -1,8 +0,0 @@
-from .connection import connect_mqtt
-
-
-def send_message(topic, payload):
-    client = connect_mqtt()
-    client.publish(topic, payload)
-    client.disconnect()
-    client.connected_flag = False
